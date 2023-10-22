1a47055bd96011328b43d8658283ad9f36d1750f
vercatalano1 <ver.catalano1@stud.uniroma3.it>
2023-10-22 20:05:02 +0200

    personaggio con salti

--- Assets/Veronica C/script/PlayerController.cs
+++ Assets/Veronica C/script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -16,10 +17,31 @@ public class PlayerController : MonoBehaviour
 
     [Header("Player Animator")]
     public CharacterController CC;
+    public float surfaceCheckRadius = 0.1f;
+    public Vector3 surfaceCheckOffset;
+    public LayerMask surfaceLayer;
+    bool onSurface;
+    
+    [SerializeField] float fallingSpeed;
+    [SerializeField] Vector3 moveDir;
 
     public void Update()
     {
+        if (onSurface)
+        {
+            fallingSpeed = -0.5f;
+        }
+        else
+        {
+            fallingSpeed += Physics.gravity.y * Time.deltaTime;
+        }
+
+        var velocity = moveDir * movementSpeed;
+        velocity.y = fallingSpeed;
+
         PlayerMovement();
+        SurfaceCheck();
+        Debug.Log("Player on Surface" + onSurface);
     }
 
     void PlayerMovement()
@@ -33,14 +55,27 @@ public class PlayerController : MonoBehaviour
 
         var movementDirection = MCC.flatRotation * movementInput;
 
+       CC.Move(movementDirection * movementSpeed * Time.deltaTime);
+
         if (movementAmount > 0)
         {
-             CC.Move(movementDirection * movementSpeed * Time.deltaTime);
             requireRotation = Quaternion.LookRotation(movementDirection);
         }
+
+       movementDirection = moveDir;
 
         transform.rotation = Quaternion.RotateTowards(transform.rotation, requireRotation,rotSpeed *Time.deltaTime);
         animator.SetFloat("movementValue", movementAmount,0.2f, Time.deltaTime);
     }
 
+    void SurfaceCheck()
+    {
+        onSurface = Physics.CheckSphere(transform.TransformPoint(surfaceCheckOffset), surfaceCheckRadius, surfaceLayer);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.TransformPoint(surfaceCheckOffset), surfaceCheckRadius);
+    }
 }
